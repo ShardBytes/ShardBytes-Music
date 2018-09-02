0deb93ca35c4eaf516d77f845ac37f3cbdd1a29c
Plajdo <31418257+Plajdo@users.noreply.github.com>
2018-09-02 13:50:33 +0200

    more project changes

--- Client/src/com/shardbytes/music/client/Client.java
+++ Client/src/com/shardbytes/music/client/Client.java
@@ -1,4 +1,9 @@
 package com.shardbytes.music.client;
 
 public class Client{
+	
+	public static void main(String[] args){
+		System.out.println("henlo");
+	}
+	
 }
--- Server/src/com/shardbytes/music/server/Server.java
+++ Server/src/com/shardbytes/music/server/Server.java
@@ -1,4 +1,9 @@
 package com.shardbytes.music.server;
 
 public class Server{
+	
+	public static void main(String[] args){
+		System.out.println("henlo server");
+	}
+	
 }
