75b6acd22b6670ce06c3e949cf07f275be415242
Plajdo <31418257+Plajdo@users.noreply.github.com>
2018-09-02 15:11:28 +0200

    Basic UI

--- Server/src/com/shardbytes/music/server/Server.java
+++ Server/src/com/shardbytes/music/server/Server.java
@@ -1,9 +1,27 @@
 package com.shardbytes.music.server;
 
 public class Server{
-	
+
+	/**
+	 * Server main method
+	 * @param args Command line arguments
+	 */
 	public static void main(String[] args){
-		System.out.println("henlo server");
+		new Server().start();
+	}
+
+	/**
+	 * Starts the server
+	 */
+	private void start(){
+		createUI();
+	}
+
+	/**
+	 * Creates server's UI
+	 */
+	private void createUI(){
+		new ServerUI().start();
 	}
 	
 }
