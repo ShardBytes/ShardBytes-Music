7fe803fc29764b3c95634aba6c263eb53f1e1741
Plajdo <31418257+Plajdo@users.noreply.github.com>
2018-09-03 15:58:41 +0200

    Client swing crap

--- Client/src/com/shardbytes/music/client/Client.java
+++ Client/src/com/shardbytes/music/client/Client.java
@@ -1,5 +1,15 @@
 package com.shardbytes.music.client;
 
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.GridBagLayout;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
@@ -7,28 +17,84 @@ import java.net.Socket;
 
 public class Client{
 	
+	private Socket socket;
+	private ObjectOutputStream toServer;
+	private ObjectInputStream fromServer;
+	
+	//Swing
+	private JTextField nicknameField;
+	
 	public static void main(String[] args){
+		
+		new Client().startGUI();
+		
+	}
+	
+	/**
+	 * Quick, dirty and temporary Swing UI for better control
+	 */
+	private void startGUI(){
+		JFrame frame = new JFrame("ShardBytes Music client");
+		frame.setSize(800, 600);
+		frame.setLayout(new FlowLayout());
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		JButton connectButton = new JButton("Connect to server");
+		connectButton.addActionListener(this::connectToServer);
+		frame.getContentPane().add(connectButton);
+		
+		JButton disconnectButton = new JButton("Disconnect from server");
+		disconnectButton.addActionListener(this::disconnectFromServer);
+		frame.getContentPane().add(disconnectButton);
+		
+		JLabel nicklabel = new JLabel("Nickname:");
+		frame.getContentPane().add(nicklabel);
+		
+		nicknameField = new JTextField("");
+		nicknameField.setPreferredSize(new Dimension(200, nicknameField.getPreferredSize().height));
+		frame.getContentPane().add(nicknameField);
+		
+		JButton send0 = new JButton("Send 0");
+		send0.addActionListener((e) -> sendMessage(0));
+		frame.getContentPane().add(send0);
+		
+		frame.setVisible(true);
+	}
+	
+	private void connectToServer(ActionEvent event){
 		try{
-			Socket socket = new Socket("localhost", 8192);
-			ObjectOutputStream toServer = new ObjectOutputStream(socket.getOutputStream());
-			ObjectInputStream fromServer = new ObjectInputStream(socket.getInputStream());
+			socket = new Socket("192.168.100.166", 8192);
+			toServer = new ObjectOutputStream(socket.getOutputStream());
+			fromServer = new ObjectInputStream(socket.getInputStream());
 			
-			while(true){
-				String message = (String)fromServer.readObject();
-				System.out.println("message = " + message);
-				toServer.writeObject("cykablyatname");
-				if(message.equals("getNickname")){
-					toServer.writeObject(0);
-					System.out.println("wrajtbajt");
-					Thread.sleep(5000);
-					toServer.writeObject(60);
-					System.out.println("dic");
-					
-				}
-				
+			String message = getMessage();
+			if(message.equals("getNickname")){
+				sendMessage(nicknameField.getText());
 			}
 			
-		}catch(IOException | InterruptedException | ClassNotFoundException e){
+		}catch(IOException e){
+			System.err.println(e.getMessage());
+		}
+		
+	}
+	
+	private void disconnectFromServer(ActionEvent event){
+		sendMessage(60);
+	}
+	
+	private String getMessage(){
+		try{
+			return (String)fromServer.readObject();
+		}catch(IOException | ClassNotFoundException e){
+			System.err.println(e.getMessage());
+		}
+		return null;
+	}
+	
+	private void sendMessage(Object message){
+		try{
+			toServer.writeObject(message);
+		}catch(IOException e){
 			System.err.println(e.getMessage());
 		}
 		
