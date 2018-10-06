cf5030c661e6a555baefc61dd3b94b7012ceb9f9
plajdo <filipsasala@outlook.com>
2018-10-06 22:49:39 +0200

    Java 8 compatible... what the heck IDEA?

--- ClientFX/src/com/shardbytes/music/client/audio/AudioPlayer.java
+++ ClientFX/src/com/shardbytes/music/client/audio/AudioPlayer.java
@@ -1,21 +1,15 @@
-package com.shardbytes.music;
+package com.shardbytes.music.client.audio;
 
 import com.shardbytes.music.client.technicalUI.JFXPlayer;
 import com.shardbytes.music.common.Song;
+import javazoom.jl.decoder.JavaLayerException;
 import javazoom.jl.player.Player;
 
 import javax.sound.sampled.AudioFormat;
-import javax.sound.sampled.AudioInputStream;
-import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
 import javax.sound.sampled.DataLine;
-import javax.sound.sampled.LineUnavailableException;
 import javax.sound.sampled.SourceDataLine;
-import javax.sound.sampled.UnsupportedAudioFileException;
-import java.io.BufferedInputStream;
 import java.io.ByteArrayInputStream;
-import java.io.IOException;
-import java.io.InputStream;
 
 public class AudioPlayer{
 	
@@ -24,8 +18,9 @@ public class AudioPlayer{
 		return ourInstance;
 	}
 	private AudioPlayer(){}
-	
-	private AudioInputStream inputStream;
+
+	private ByteArrayInputStream inputStream;
+	//private AudioInputStream inputStream;
 	private AudioFormat format;
 	private DataLine.Info dataInfo;
 	private SourceDataLine line;
@@ -36,60 +31,53 @@ public class AudioPlayer{
 	private boolean paused = false;
 	private int currentPause = 0;
 	
-	public void preloadAsBytes(ByteArrayInputStream songByteStream) throws IOException, UnsupportedAudioFileException, LineUnavailableException{
+	public void preloadAsBytes(ByteArrayInputStream songByteStream) throws JavaLayerException{
 		if(playing){
 			stop();
 		}
-		
+
+		/*
 		inputStream = AudioSystem.getAudioInputStream(songByteStream);
 		format = constructNewAudioFormat(inputStream.getFormat());
 		dataInfo = new DataLine.Info(SourceDataLine.class, format);
 		line = (SourceDataLine)AudioSystem.getLine(dataInfo);
 		line.open();
-		
+		*/
+
+		inputStream = songByteStream;
+		player = new Player(inputStream);
+
 	}
 	
-	public void preloadAsBytes(ByteArrayInputStream songByteStream, Song songData) throws UnsupportedAudioFileException, IOException, LineUnavailableException{
+	public void preloadAsBytes(ByteArrayInputStream songByteStream, Song songData) throws JavaLayerException{
 		preloadAsBytes(songByteStream);
 		JFXPlayer.getController().setSongData(songData);
 	}
 	
-	public void getFromStream(InputStream stream) throws IOException, UnsupportedAudioFileException, LineUnavailableException{
-		if(playing){
-			stop();
-		}
-		
-		inputStream = AudioSystem.getAudioInputStream(new BufferedInputStream(stream));
-		format = constructNewAudioFormat(inputStream.getFormat());
-		dataInfo = new DataLine.Info(SourceDataLine.class, format);
-		line = (SourceDataLine)AudioSystem.getLine(dataInfo);
-		line.open();
-		
-	}
-	
-	public void play(){
+	public void play() throws JavaLayerException{
 		currentPause = 0;
 		playing = true;
-		line.start();
+		player.play();
 		
 	}
 	
 	public void stop(){
 		currentPause = 0;
 		playing = false;
-		line.stop();
+		player.close();
 		
 	}
 	
-	public void pause(){
+	public void pause() throws JavaLayerException{
 		if(playing && !paused){
 			paused = true;
-			currentPause = line.getFramePosition();
-			line.stop();
+			currentPause = player.getPosition();
+			player.close();
 			
 		}else if(playing && paused){
 			paused = false;
-			line.start();
+			player = new Player(inputStream);
+			player.play(currentPause);
 			
 		}
 		
---
+++ ClientFX/src/com/shardbytes/music/client/audio/PlayerQueue.java
@@ -0,0 +1,12 @@
+package com.shardbytes.music.client.audio;
+
+import com.shardbytes.music.common.Song;
+
+import java.util.ArrayList;
+
+public class PlayerQueue{
+
+	//TODO: actual wrapper and stuff
+	public ArrayList<Song> queue = new ArrayList<>();
+
+}
